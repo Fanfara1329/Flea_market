9d7723e5bd60a3e26af50b5c1a062058126b265c
OtterAlexandra <otter9919@mail.ru>
2022-04-10 16:15:13 +0300

    post request

--- main.py
+++ main.py
@@ -8,21 +8,24 @@ db_session.global_init("db/flea.db")
 app.config['SECRET_KEY'] = 'abobys'
 
 
-@app.route('/registration')
+@app.route('/registration', methods=['POST', 'GET'])
 def registration():
     form = forms.RegistrationForm()
     if form.validate_on_submit():
-        user = User(name=form.name.date,
+        print(90)
+
+    elif request.method == 'POST':
+        user = User(name=form.name.data,
                     email=form.email.data,
                     city=form.city.data,
                     cards=form.cards.data,
                     password=form.password.data,
                     gender=form.gender.data)
-        # s = db_session.create_session()
-        # s.add(user)
-        # s.commit()
-        # s.close()
-
+        s = db_session.create_session()
+        s.add(user)
+        s.commit()
+        s.close()
+        return "Отправлена"
     return render_template('registration.html', title='Registration', form=form)
 
 
